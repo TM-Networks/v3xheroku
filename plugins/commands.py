43101f483579499e66702a3af17006ddc75f2a5d
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-13 13:50:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,9 +47,6 @@ async def start(client, message):
             InlineKeyboardButton('🍃 sᴜʀᴘʀɪsᴇ 🍃', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgIAAxkBAAEGrmxjjNyUm6AUgsjrwt4EcpvlY3pMzwACZBgAAvG1EEmY4_NunlJkaysE")
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -68,9 +65,6 @@ async def start(client, message):
             InlineKeyboardButton('🍃 sᴜʀᴘʀɪsᴇ 🍃', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgIAAxkBAAEGrmxjjNyUm6AUgsjrwt4EcpvlY3pMzwACZBgAAvG1EEmY4_NunlJkaysE")
-        await asyncio.sleep(1)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
